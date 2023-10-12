894783a5a2876950bf2e25e40f0965a6b804325c
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2023-10-12 10:42:43 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,10 +30,10 @@ app.use("/v1/users", verifyToken, userRouter);
 app.use("/v1/students", verifyToken, studentRouter);
 app.use("/v1/scores", verifyToken, scoreRouter);
 
-app.get("/*", (req, res) => {
-  res.status(404).redirect("error.html")
+//app.get("/*", (req, res) => {
+//  res.status(404).redirect("error.html")
   //res.status(404).sendFile(__dirname + "error.html")
-})
+//})
 
 const server = () => {
   db(process.env.DB_URI)
