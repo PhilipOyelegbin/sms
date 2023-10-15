88d6f2924daf1d29384a929aed8062b74fc9f98d
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-10-15 16:44:10 +0100

    feat: the admin login is now functional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import { lazy, Suspense } from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import ProtectedRoutes from './utilities/ProtectedRoutes';
-// import SharedLayout from './utilities/SharedLayout';
-// import Loader from './components/Loader';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ProtectedRoutes from './components/ProtectedRoutes';
 
 const Home = lazy(() => import('./pages/landing_page/home'));
 const AdminLogin = lazy(() => import('./pages/login_page/adminLogin'));
 const UserLogin = lazy(() => import('./pages/login_page/userLogin'));
 const AdminRegistration = lazy(() => import('./pages/registration_page/AdminRegistration'));
 const UserRegistration = lazy(() => import('./pages/registration_page/UserRegistration'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 
 function App() {
   const router = createBrowserRouter(
@@ -21,9 +20,9 @@ function App() {
         <Route path='login/student' element={<UserLogin/>}/>
         <Route path='register/admin' element={<AdminRegistration/>}/>
         <Route path='register/student' element={<UserRegistration/>}/>
-        {/* <Route element={<ProtectedRoutes/>}>
-          <Route path='write' element={<Write/>}/>
-        </Route> */}
+        <Route element={<ProtectedRoutes/>}>
+          <Route path='dashboard' element={<Dashboard/>}/>
+        </Route>
       </Route>
     )
   );
@@ -31,7 +30,7 @@ function App() {
   return (
     <Suspense>
       <RouterProvider router={router}/>
-      {/* <ToastContainer position='top-right' autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover /> */}
+      <ToastContainer position='top-right' autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
     </Suspense>
   )
 }
--- frontend/src/pages/login_page/AdminLogin.jsx
+++ frontend/src/pages/login_page/AdminLogin.jsx
@@ -1,26 +1,53 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 import admin_image from '../../assets/admin.jpg'
 import "./login.css"
 
+
 function AdminLogin() {
+  const [formData, setFormData] = useState({email: "", password: ""})
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData({...formData, [name]: value})
+  }
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async(e) => {
+    try {
+      e.preventDefault()
+      const result = await axios.post(import.meta.env.VITE_APP_ADMIN_AUTH_URL, formData)
+      setFormData({email: "", password: ""})
+      toast.success("Login successfully...")
+      sessionStorage.setItem("token", result.data.user)
+      navigate("/dashboard")
+    } catch (error) {
+      error.message && toast.error("Invalid email or password!")
+    }
+  }
+
   return (
     <article className="login-page">
-      <div class="login-container">
-        <div class="left-column">
+      <div className="login-container">
+        <div className="left-column">
           <h2>Admin Login</h2>
-          <form>
-            <div class="form-group">
-              <label for="email">Email:</label>
-              <input type="email" id="email" name="email" required/>
+          <form onSubmit={handleSubmit} autoComplete='off'>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} minLength="10"  placeholder="Enter your email" required/>
             </div>
-            <div class="form-group">
-              <label for="password">Password:</label>
-              <input type="password" id="password" name="password" required/>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} minLength="6"  placeholder="Enter your password" required/>
             </div>
             <button type="submit">Login</button>
           </form>
         </div>
 
-        <div class="right-column">
+        <div className="right-column">
           <img src={admin_image} alt="a focused woman behind the laptop"/>
         </div>
       </div>
