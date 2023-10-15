a8212e307178becf3a5af2bb6734fe2093ecfd02
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-10-15 15:10:32 +0100

    update: the cors origin updated to allow the frontend

--- server/index.js
+++ server/index.js
@@ -14,12 +14,13 @@ const {verifyToken} = require("./src/authentication/auth.middleware")
 const app = express()
 
 const corsOptions = {
-  origin: [
-    "http://localhost:4000/*",
-    "http://localhost:5173/*",
-    "https://studentmanagementsystem-api.vercel.app/*",
-    "https://studentsmanagementsystem.netlify.app/*"
-  ],
+  // origin: [
+    //   "http://localhost:5173",
+    //   "http://localhost:4000/*",
+    //   "https://studentsmanagementsystem.netlify.app/*",
+  //   "https://studentmanagementsystem-api.vercel.app/*"
+  // ],
+  origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204
